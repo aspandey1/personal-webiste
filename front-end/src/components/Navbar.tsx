864c2c4835b4b09bc3e5a42cc1df33c9f3a85a2a
Ashish Pandey <ashish.apandey@hotmail.com>
2024-01-06 00:11:03 -0600

    Redesigned the navbar to make it better for smaller screens

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -1,35 +1,65 @@
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+
 export default function NavBar() {
+  interface Links {
+    name: string;
+    link: string;
+  }
+
+  const navLinks: Links[] = [
+    { name: "HOME", link: "#home" },
+    { name: "PROJECTS", link: "#projects" },
+    { name: "CERTIFICATES", link: "#certifications" },
+    { name: "EDUCATION", link: "#education" },
+  ];
+
+  const [navOpen, setNavOpen] = useState<Boolean>(false);
+
   return (
     <>
-      <header className="md:block hidden bg-blue-500 py-6 flex items-center flex-col">
-        <ul className="flex flex-row pt-5">
-          <li className="w-[8.5rem] w-full mx-2 bg-red-100 text-lg rounded-lg text-center">
-            <a href="#about" className="block px-4 py-2 font-semibold">
-              Home
-            </a>
-          </li>
-          <li className="w-[8.5rem] mx-2 bg-red-100 text-lg rounded-lg text-center">
-            <a href="#projects" className="block px-4 py-2 font-semibold">
-              Projects
-            </a>
-          </li>
-          <li className="w-[8.5rem] mx-2  bg-red-100 text-lg rounded-lg text-center">
-            <a href="#education" className="block px-4 py-2 font-semibold">
-              Education
-            </a>
-          </li>
-          <li className="w-[8.5rem] mx-2  bg-red-100 text-lg rounded-lg text-center">
-            <a href="#certificates" className="block px-4 py-2 font-semibold">
-              Certificates
-            </a>
-          </li>
-          <li className="w-[8.5rem] mx-2  bg-red-100 text-lg rounded-lg text-center">
-            <a href="#contact" className="block px-4 py-2 font-semibold">
-              Contact Me
-            </a>
-          </li>
-        </ul>
-      </header>
+      <div className="shadow-md w-full">
+        <div className="md:flex justify-between bg-blue-600 py-4 md:px-10 px-7">
+          <div className="font-bold text-2xl flex items-center">
+            Ashish Pandey
+            {navOpen ? (
+              <IoClose
+                onClick={() => {
+                  setNavOpen(!navOpen);
+                }}
+                className="md:hidden absolute right-8 cursor-pointer"
+              />
+            ) : (
+              <IoMenu
+                onClick={() => {
+                  setNavOpen(!navOpen);
+                }}
+                className="md:hidden absolute right-8 cursor-pointer"
+              />
+            )}
+          </div>
+
+          <ul
+            className={`md:flex md:items-center md:pb-0 pb-2 bg-red-400 absolute md:static md:z-auto z-[-1] right-0 w-full md:w-auto md:pl-0 pl-7 transition-all duration-200 ease-in ${
+              navOpen
+                ? "top-[86vh] opacity-100"
+                : "top-[60vh] md:opacity-100 opacity-0"
+            }`}
+          >
+            {navLinks.map((link) => (
+              <li key={link.name} className="md:ml-8 md:my-0 my-7 text-lg">
+                <a
+                  href={link.link}
+                  className="text-gray-800 hover:text-gray-400 duration-500"
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
